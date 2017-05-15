6f42cb43582d599a231adc80b6b90c280cdf7270
jrugis <j.rugis@auckland.ac.nz>
2017-05-15 17:35:18 +1200

    lots

--- images/cropping.sh
+++ images/cropping.sh
@@ -22,5 +22,13 @@ ffmpeg -i cell1N-10.png -vf crop=1300:1050:420:60 cell1N-10_crop.png
 #ffmpeg -i seven_cells_smooth.png -vf "crop=1150:1080:400:60" seven_cells_smooth_crop.png
 #ffmpeg -i three_cells_smooth.png -vf "crop=1150:1080:400:60" three_cells_smooth_crop.png
 
+ffmpeg -i cell1-C0.png -vf "crop=1090:850:480:110" cell1-C0_crop.png
+ffmpeg -i cell1-C6.png -vf "crop=1090:850:480:110" cell1-C6_crop.png
+ffmpeg -i cell1-C16.png -vf "crop=1090:850:480:110" cell1-C16_crop.png
+ffmpeg -i cell1-C40.png -vf "crop=1090:850:480:110" cell1-C40_crop.png
+ffmpeg -i cell1-C100.png -vf "crop=1090:850:480:110" cell1-C100_crop.png
 
-
+ffmpeg -i cell1-NC6.png -vf "crop=1090:850:480:110" cell1-NC6_crop.png
+ffmpeg -i cell1-NC16.png -vf "crop=1090:850:480:110" cell1-NC16_crop.png
+ffmpeg -i cell1-NC40.png -vf "crop=1090:850:480:110" cell1-NC40_crop.png
+ffmpeg -i cell1-NC100.png -vf "crop=1090:850:480:110" cell1-NC100_crop.png
